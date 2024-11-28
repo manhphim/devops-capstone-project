309642e590d542fcb4860a3cdd0f2d8e686b2305
Minh Pham <ngocminhpham66@gmail.com>
2024-11-28 16:34:02 -0500

    lint fix

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -127,7 +127,6 @@ class TestAccountService(TestCase):
             content_type="test/html"
         )
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
-
     # ADD YOUR TEST CASES HERE ...
 
     def test_get_account_not_found(self):
